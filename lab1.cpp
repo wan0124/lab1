eb7c87f2a85ecd13c54103a8f8ef064ba6d47499
Wan <jcps3503@gmail.com>
2016-03-15 01:19:51 +0800

    lab1

---
+++ lab1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     int n;
+     cin>>n;
+
+     while(n!=1){
+         cout<<n;
+         if(n==1)
+             return 0;
+         else if(n%2==1)
+             n=3*n+1;
+         else
+             n=n/2;
+     }
+}
+
